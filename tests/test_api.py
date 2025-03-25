017af5170fd944394f83674d18a62cab313139f8
Julien Cantalapiedra <jcantalapiedra1@gmail.com>
2025-03-25 16:44:12 +0100

    🐛 Corrige test_predict_endpoint pour téléchargement dynamique du modèle

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
-import pytest
 from fastapi.testclient import TestClient
 from api.main import app
+from unittest.mock import patch
+import pytest
 from pathlib import Path
 
 client = TestClient(app)
@@ -14,16 +15,15 @@ def test_root():
     not Path("tests/frankfurt_000000_001236_leftImg8bit.png").exists(),
     reason="Image de test absente."
 )
-def test_predict_endpoint():
-    image_path = Path("tests/frankfurt_000000_001236_leftImg8bit.png")
+@patch('api.main.predict_mask')
+def test_predict_endpoint(mock_predict_mask):
+    # Simuler une prédiction du modèle sans le charger
+    mock_predict_mask.return_value = (np.zeros((256, 512), dtype=np.uint8))
 
+    image_path = Path("tests/frankfurt_000000_001236_leftImg8bit.png")
     with open(image_path, "rb") as img:
         files = {"image_file": ("test_image.png", img, "image/png")}
         response = client.post("/predict", files=files)
 
     assert response.status_code == 200
     assert response.headers["content-type"] == "image/png"
-
-    # Vérifier si le résultat est bien une image PNG valide
-    content = response.content
-    assert content[:8] == b'\x89PNG\r\n\x1a\n', "La réponse n'est pas une image PNG valide"
